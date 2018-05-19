94dec70a8451a75fc3bcb106ca19d20ebce516f8
Touchy69 <gear2and3@free.fr>
2018-05-19 20:01:57 +0200

    Ajout de 4 champs dans l'Entity Objet

--- src/Entity/Objet.php
+++ src/Entity/Objet.php
@@ -32,6 +32,36 @@ class Objet
      */
     private $image;
     /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Serializer\Expose
+     */
+    private $image_on;
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Serializer\Expose
+     */
+    private $image_off;
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Serializer\Expose
+     */
+    private $image_min;
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Serializer\Expose
+     */
+    private $image_med;
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Serializer\Expose
+     */
+    private $image_max;
+    /**
      * @ORM\ManyToOne(targetEntity="App\Entity\AttributObjet", inversedBy="objets")
      * @ORM\JoinColumn(nullable=false)
      * 
@@ -66,6 +96,51 @@ class Objet
     public function setImage(string $image): self
     {
         $this->image = $image;
+        return $this;
+    }
+    public function getImageOn(): ?string
+    {
+        return $this->image_on;
+    }
+    public function setImageOn(string $image_on): self
+    {
+        $this->image_on = $image_on;
+        return $this;
+    }
+    public function getImageOff(): ?string
+    {
+        return $this->image_off;
+    }
+    public function setImageOff(string $image_off): self
+    {
+        $this->image_off = $image_off;
+        return $this;
+    }
+    public function getImageMin(): ?string
+    {
+        return $this->image_min;
+    }
+    public function setImageMin(string $image_min): self
+    {
+        $this->image_min = $image_min;
+        return $this;
+    }
+    public function getImageMed(): ?string
+    {
+        return $this->image_med;
+    }
+    public function setImageMed(string $image_med): self
+    {
+        $this->image_med = $image_med;
+        return $this;
+    }
+    public function getImageMax(): ?string
+    {
+        return $this->image_max;
+    }
+    public function setImageMax(string $image_max): self
+    {
+        $this->image_max = $image_max;
         return $this;
     }
     public function getAttributObjet(): ?AttributObjet
